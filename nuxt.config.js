ac69a6899cccba217c5539724d23eb669611c7b5
yannick lescure <yannick.lescure@gmail.com>
2021-02-22 22:32:09 -0500

    improve code + fix bugs

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,10 +11,90 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'Yannick Lescure, France'
+      },
+
+      // Twitter
+      // Test on: https://cards-dev.twitter.com/validator
+      {
+        hid: 'twitter:card',
+        name: 'twitter:card',
+        content: 'summary_large_image'
+      },
+      { hid: 'twitter:site', name: 'twitter:site', content: '@yannicklescure' },
+      {
+        hid: 'twitter:url',
+        name: 'twitter:url',
+        content: 'https://yannicklescure.com'
+      },
+      {
+        hid: 'twitter:title',
+        name: 'twitter:title',
+        content: 'Yannick Lescure'
+      },
+      {
+        hid: 'twitter:description',
+        name: 'twitter:description',
+        content:
+          'Yannick Lescure, France'
+      },
+      {
+        hid: 'twitter:image',
+        name: 'twitter:image',
+        content: '/profile_avatar.jpg'
+      },
+
+      // Open Graph
+      // Test on: https://developers.facebook.com/tools/debug/
+      { hid: 'og:site_name', property: 'og:site_name', content: 'Yannick Lescure' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      {
+        hid: 'og:url',
+        property: 'og:url',
+        content: 'https://yannicklescure.com'
+      },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: 'Yannick Lescure, France'
+      },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content:
+          'Yannick Lescure, France'
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: '/profile_avatar.jpg'
+      },
+      {
+        hid: 'og:image:secure_url',
+        property: 'og:image:secure_url',
+        content: '/profile_avatar.jpg'
+      },
+      {
+        hid: 'og:image:alt',
+        property: 'og:image:alt',
+        content: 'Yannick Lescure'
+      }
+
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    // canonical
+    link: [
+      {
+        hid: 'canonical',
+        rel: 'canonical',
+        href: `https://yannicklescure.com`
+      }
     ]
   },
 
